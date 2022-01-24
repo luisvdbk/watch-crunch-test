5a26c1b3114e49c7d3e763be146368f1b2372b1f
Luis van den Bussche <luisvdbk312@gmail.com>
2022-01-24 20:49:40 +0100

    factories and seeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Post;
+use App\Models\User;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(),
+        'user_id' => factory(User::class),
+    ];
+});
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        collect()->times(10)->each(function () {
+            $user = factory(User::class)->create();
+            $posts = factory(Post::class, rand(1, 5))->create([
+                'user_id' => $user->id,
+            ]);
+        });
     }
 }
