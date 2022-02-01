8d72ee37a1de955454e383e6b8afe257c6a3e06d
Luis van den Bussche <luisvdbk312@gmail.com>
2022-02-01 21:37:21 +0100

    return arry rather than collection

--- app/Actions/GetTopUsers.php
+++ app/Actions/GetTopUsers.php
@@ -4,7 +4,6 @@ namespace App\Actions;
 
 use App\Models\Post;
 use App\Models\User;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Cache;
 use Spatie\QueueableAction\QueueableAction;
 
@@ -16,7 +15,7 @@ class GetTopUsers
     public const MIN_POSTS_REQUIRED = 10;
     public const CACHE_KEY = 'top_users';
 
-    public function execute(): Collection
+    public function execute(): array
     {
         /** @var \Illuminate\Database\Eloquent\Collection */
         $topUsers = Cache::remember(self::CACHE_KEY, now()->endOfDay(), function () {
@@ -28,12 +27,15 @@ class GetTopUsers
             return User::with('lastPost')->whereIn('id', $usersThatPostedEnoughWithinLastDays)->get();
         });
 
+        /**
+         * we could also make this return a dto that holds this structured data
+         */
         return $topUsers->map(function (User $user) {
             return [
                 'username' => $user->username,
                 'created_posts_count' => $user->posts_count,
                 'last_post_title' => $user->lastPost->title,
             ];
-        });
+        })->toArray();
     }
 }
